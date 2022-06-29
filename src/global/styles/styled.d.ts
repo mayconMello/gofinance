de8837ddbc5b7de79a1c35eab87ea87a5773ab52
Maycon Ferreira <mayconferr.18@gmail.com>
2022-06-28 23:14:18 -0300

    Initial Commit

---
+++ src/global/styles/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+
+import theme from './theme';
+
+declare module 'styled-components' {
+  type ThemeTypes = typeof theme
+
+  export interface DefaultTheme extends ThemeTypes { }
+}
